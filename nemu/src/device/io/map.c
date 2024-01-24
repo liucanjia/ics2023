b3083e16620b6ed0cbf868529554be3c26e8ad9e
liucanjia <liu981786891@qq.com>
2024-01-24 13:23:57 +0800

    PA2:   add code to implement dtrace

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -23,6 +23,16 @@
 static uint8_t *io_space = NULL;
 static uint8_t *p_space = NULL;
 
+#ifdef CONFIG_DTRACE
+static void display_dread(IOMap *map, paddr_t addr, int len) {
+  _Log("\nDevice %s read at " FMT_PADDR ",len is %d\n", map->name, addr, len);
+}
+
+static void display_dwrite(IOMap *map, paddr_t addr, int len, word_t data) {
+  _Log("\nDevice %s write at " FMT_PADDR ",len is %d, data is " FMT_WORD "\n", map->name, addr, len, data);
+}
+#endif
+
 uint8_t* new_space(int size) {
   uint8_t *p = p_space;
   // page aligned;
@@ -58,6 +68,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  IFDEF(CONFIG_DTRACE, display_dread(map, addr, len));
   return ret;
 }
 
@@ -67,4 +78,5 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
+  IFDEF(CONFIG_DTRACE, display_dwrite(map, addr, len, data));
 }
