71f274c1d1aafe3e694892cbf1e5a4030c4ee0ab
liucanjia <liu981786891@qq.com>
2024-02-26 14:42:39 +0800

    PA3:   add code to implement strace

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,14 +9,19 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
+  uintptr_t return_val;
+
   switch (a[0]) {
     case SYS_exit:
+      printf("Syscall exit, arg: %d\n", a[0]);
       halt(a[1]);
       break;
     case SYS_yield:
       yield();
-      c->GPRx = 0;
+      return_val = 0;
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  printf("Syscall No: %d, arg1: %d, arg2: %d, arg3: %d, arg4: %d, arg5: %d, arg6: %d, arg7: %d, return value: %d.\n", c->GPR1, c->GPR2, c->GPR3, c->GPR4, c->GPR5, c->GPR6, c->GPR7, c->GPR8, c->GPRx);
+  c->GPRx = return_val;
 }
