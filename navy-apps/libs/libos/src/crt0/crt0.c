797bd42ebf91a0698814d9b7b87d77cb891ca89a
liucanjia <liu981786891@qq.com>
2024-03-05 22:48:54 +0800

    PA3:   add code to support running fceux in nanos-lite

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -3,10 +3,12 @@
 #include <assert.h>
 
 int main(int argc, char *argv[], char *envp[]);
+// int main(const char *args);
 extern char **environ;
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
   exit(main(0, empty, empty));
+  // exit(main("mario"));   // run mario in nanos-lite
   assert(0);
 }
